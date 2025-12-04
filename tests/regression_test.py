80dfb5bff69c10c59d8ed5d0a20ad278b9eb4f45
Jonathan Wehrle <jonathan@pengubytes.de>
2025-12-04 19:10:54 +0100

    generic regression test

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -8,35 +8,19 @@ from aoc25.__main__ import main
 
 
 class RegressionTests(TestCase):
-    def setUp(self):
+    def test_regressions(self):
         answers_path = Path(__file__).parent.parent / "answers.json"
         with answers_path.open() as answers_file:
-            self.answers = json.load(answers_file)
+            answers = json.load(answers_file)
 
-    def test_day01_part1(self):
-        input_path = Path(__file__).parent.parent / "inputs" / "day01.txt"
-        with redirect_stdout(StringIO()) as buffer:
-            main(input_path, day=1, part=1)
+        for key, expected in answers.items():
+            day = int(key[3:5])
+            part = int(key[-1:])
+            with self.subTest(day=day, part=part):
+                input_path = Path(__file__).parent.parent / \
+                    "inputs" / f"day{day:02d}.txt"
 
-        self.assertEqual(buffer.getvalue(), f"{self.answers["day01part1"]}\n")
+                with redirect_stdout(StringIO()) as buffer:
+                    main(input_path, day=day, part=part)
 
-    def test_day01_part2(self):
-        input_path = Path(__file__).parent.parent / "inputs" / "day01.txt"
-        with redirect_stdout(StringIO()) as buffer:
-            main(input_path, day=1, part=2)
-
-        self.assertEqual(buffer.getvalue(), f"{self.answers["day01part2"]}\n")
-
-    def test_day02_part1(self):
-        input_path = Path(__file__).parent.parent / "inputs" / "day02.txt"
-        with redirect_stdout(StringIO()) as buffer:
-            main(input_path, day=2, part=1)
-
-        self.assertEqual(buffer.getvalue(), f"{self.answers["day02part1"]}\n")
-
-    def test_day02_part2(self):
-        input_path = Path(__file__).parent.parent / "inputs" / "day02.txt"
-        with redirect_stdout(StringIO()) as buffer:
-            main(input_path, day=2, part=2)
-
-        self.assertEqual(buffer.getvalue(), f"{self.answers["day02part2"]}\n")
+                self.assertEqual(int(buffer.getvalue().strip()), expected)
